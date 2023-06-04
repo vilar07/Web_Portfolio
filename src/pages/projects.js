5fd1fac1dabc0e0281b631229f925e142e58318c
Tiago Vilar <tiagovilar07@ua.pt>
2023-06-04 01:58:45 +0100

    projects-page

---
+++ src/pages/projects.js
@@ -0,0 +1,146 @@
+import React from 'react'
+import Head from 'next/head'
+import Layout from '@/components/Layout'
+import AnimatedText from '@/components/AnimatedText'
+import Link from 'next/link'
+import Image from 'next/image'
+import { GithubIcon } from '@/components/Icons'
+import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg"
+
+
+const FeaturedProject = ({type, title, summary, img, link = '', github = ''}) => {
+    return(
+        <article className='w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative
+        rounded-br-2xl
+        '>
+            <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl'/>
+            <Link href={link} target="_blank"
+            className='w-1/2 cursor-pointer overflow-fidden rounded-lg'
+            >
+                <Image src={img} alt={title} className='w-full h-auto'/>
+            </Link>
+
+            <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
+                    <span className='text-green font-medium text-xl'>{type}</span>
+                    <Link href={link} target="_blank" className='hover:underline underline-offset-2' >
+                        <h2 className='my-2 w-full text-left text-4xl font-bold'>{title}</h2>
+                    </Link>
+                
+                <p className='my-2 font-medium text-dark'>{summary}</p>
+                <div className='mt-2 flex items-center'>
+                    <Link href={github} target="_blank" className='w-10'><GithubIcon/></Link>
+                    <Link href={link} target="_blank" className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold'>Visit Project</Link>
+                </div>
+            </div>
+        </article>
+    )
+}
+
+const Project = ({type, title, img, link = '', github = ''}) => {
+    return(
+        <article className='w-full flex flex-col items-center justify-center rounded-2xl border
+        border-solid border-dark bg-light p-6 relative
+        '>
+            <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl'/>
+            <Link href={link} target="_blank"
+            className='w-full cursor-pointer overflow-fidden rounded-lg'
+            >
+                <Image src={img} alt={title} className='w-full h-auto'/>
+            </Link>
+
+            <div className='w-full flex flex-col items-start justify-between mt-4'>
+                    <span className='text-green font-medium text-xl'>{type}</span>
+                    <Link href={link} target="_blank" className='hover:underline underline-offset-2' >
+                        <h2 className='my-2 w-full text-left text-3xl font-bold'>{title}</h2>
+                    </Link>
+
+                <div className='w-full mt-2 flex items-center justify-between'>
+                    <Link href={link} target="_blank" className='text-lg font-semibold underline'>Visit</Link>
+                    <Link href={github} target="_blank" className='w-8'><GithubIcon/></Link>
+                    
+                </div>
+            </div>
+        </article>
+    )
+}
+
+
+const projects = () => {
+  return (
+    <>
+        <Head>
+            <title>Tiago Vilar | Projects Page</title>
+            <meta name='description' content='Projects Page' />
+        </Head>
+        <main className='w-full mb-16 flex flex-col items-center justify-center'>
+            <Layout className='pt-16'>
+                <AnimatedText text="Imagination Trumps Knowledge!" className='text-center mb-16'/>
+
+                <div className='grid grid-cols-12 gap-24 gap-y-32'>
+                    <div className='col-span-12'>
+                        <FeaturedProject
+                        title="Web Portfolio"
+                        summary="This is my web portfolio. It was built using Next.js, Tailwind CSS, and Framer Motion."
+                        link="/"
+                        type="Featured Project"
+                        img={project1}
+                        github="https://github.com/vilar07/Web_Portfolio"
+                        />
+                    </div>
+                    <div className='col-span-6'>
+                        <Project
+                        title="Web Portfolio"
+                        link="/"
+                        type="Featured Project"
+                        img={project1}
+                        github="https://github.com/vilar07/Web_Portfolio"
+                        />
+                    </div>
+                    <div className='col-span-6'>
+                        <Project
+                        title="Web Portfolio"
+                        link="/"
+                        type="Featured Project"
+                        img={project1}
+                        github="https://github.com/vilar07/Web_Portfolio"
+                        />
+                    </div>
+                    <div className='col-span-12'>
+                        <FeaturedProject
+                        title="Web Portfolio"
+                        summary="This is my web portfolio. It was built using Next.js, Tailwind CSS, and Framer Motion."
+                        link="/"
+                        type="Featured Project"
+                        img={project1}
+                        github="https://github.com/vilar07/Web_Portfolio"
+                        />
+                    </div>
+                    <div className='col-span-6'>
+                        <Project
+                        title="Web Portfolio"
+                        link="/"
+                        type="Featured Project"
+                        img={project1}
+                        github="https://github.com/vilar07/Web_Portfolio"
+                        />
+                    </div>
+                    <div className='col-span-6'>
+                        <Project
+                        title="Web Portfolio"
+                        link="/"
+                        type="Featured Project"
+                        img={project1}
+                        github="https://github.com/vilar07/Web_Portfolio"
+                        />
+                    </div>
+
+
+                </div>
+
+            </Layout>
+        </main>
+    </>
+  )
+}
+
+export default projects
