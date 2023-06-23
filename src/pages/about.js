f96edbc903f13463595ec2ef7262b3ff39c827e3
Tiago Vilar <tiagovilar07@ua.pt>
2023-06-23 23:50:25 +0100

    ghyju

--- src/pages/about.js
+++ src/pages/about.js
@@ -45,58 +45,58 @@ const about = () => {
         </Head>
         <main className='flex w-full flex-col items-center justify-center dark:text-light'>
             <Layout className='pt-16'>
-                <AnimatedText text='Passion Fuels Purpose!' className='mb-16' />
+                <AnimatedText text='Passion Fuels Purpose!' className='mb-16 text-center lg:!text-7xl sm:!text-6xl xs:!text-4sl' />
             
-            <div className='grid w-full grid-cols-8 gap-16'>
-                <div className='col-span-3 flex flex-col items-start justify-start'>
-                    <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>Biography</h2>
-                    <p className='font-medium'>
-                    - xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                    </p>
-                    <p className='my-4 font-medium'>
-                    - xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                    </p>
-                    <p className='font-medium'>
-                    - xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                    </p>
-                </div>
-                <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light'>
-                  <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light'/>
-                  <Image src={profilePicTiago} alt='Tiago Vilar' className='w-full h-auto rounded-2xl'
-                  priority
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                  />  
-                </div>
-                <div className='col-span-2 flex flex-col items-end justify-between'>
+                <div className='grid w-full grid-cols-8 gap-16 sm:gap-8 '>
+                    <div className=' col-span-3 flex flex-col items-start justify-start  xl:col-span-4 '>
+                        <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>Biography</h2>
+                        <p className='font-medium break-words'>
+                        - xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                        </p>
+                        <p className='my-4 font-medium break-words '>
+                        - xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                        </p>
+                        <p className='font-medium break-words'>
+                        - xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                        </p>
+                    </div>
+                    <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light'>
+                        <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light xl:col-span-2' />
+                        <Image src={profilePicTiago} alt='Tiago Vilar' className='w-full h-auto rounded-2xl'
+                        priority
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                        />  
+                    </div>
+                    <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center'>
 
-                    <div className='flex flex-col items-end justify-center'>
-                        <span className='inline-block text-7xl font-bold'>
-                            <AnimatedNumbers value={2} />+
-                        </span>
-                        <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>satisfied clients</h2>
-                    </div>
+                        <div className='flex flex-col items-end justify-center'>
+                            <span className='inline-block text-7xl font-bold'>
+                                <AnimatedNumbers value={2} />+
+                            </span>
+                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>satisfied clients</h2>
+                        </div>
 
-                    <div className='flex flex-col items-end justify-center'>
-                        <span className='inline-block text-7xl font-bold'>
-                        <AnimatedNumbers value={20} />+
-                        </span>
-                        <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>projects completed</h2>
-                    </div>
-                    
-                    <div className='flex flex-col items-end justify-center'>
-                        <span className='inline-block text-7xl font-bold'>
-                        <AnimatedNumbers value={1} />+
-                        </span>
-                        <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>years of experience</h2>
+                        <div className='flex flex-col items-end justify-center'>
+                            <span className='inline-block text-7xl font-bold'>
+                            <AnimatedNumbers value={20} />+
+                            </span>
+                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>projects completed</h2>
+                        </div>
+                        
+                        <div className='flex flex-col items-end justify-center'>
+                            <span className='inline-block text-7xl font-bold'>
+                            <AnimatedNumbers value={1} />+
+                            </span>
+                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>years of experience</h2>
+                        </div>
                     </div>
                 </div>
-            </div>
 
             <Skills />
             <Education/>
